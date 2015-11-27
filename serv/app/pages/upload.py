2db553ff3d65d74d288d1412d499251abac5c4b5
Jaume Martin <jaumartin@gmail.com>
2015-11-27 12:52:21 +0100

    Added upload page handler

--- serv/app/pages/upload.py
+++ serv/app/pages/upload.py
@@ -3,7 +3,19 @@
 """
 from __future__ import absolute_import
 
-from flask import Blueprint, render_template
+import os
+
+from flask import current_app as app
+from flask import Blueprint
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+from flask import abort
+
+from werkzeug import secure_filename
+
+from app.forms.upload import UploadForm
 
 
 bp = Blueprint('upload', __name__, url_prefix='/upload')
@@ -11,4 +23,20 @@ bp = Blueprint('upload', __name__, url_prefix='/upload')
 
 @bp.route('', methods=['GET'])
 def index():
-    return render_template('upload/index.html')
+    form = UploadForm()
+    return render_template('upload/index.html', form=form)
+
+@bp.route('/submit', methods=['POST'])
+def submit():
+    form = UploadForm()
+    if form.validate_on_submit():
+        filename = secure_filename(form.malware.data.filename)
+        form.malware.data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return redirect(url_for('upload.landing', filename=filename))
+
+@bp.route("/landing", methods=["GET"])
+def landing(filename=None):
+    if filename:
+        return render_template("upload/landing.html", filename)
+    abort(404)
+
