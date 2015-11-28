a878d244eb9b0d6bf9bc5a9a772d01262ecd8179
M. Sánchez <marsncm1@gmail.com>
2015-11-28 07:28:19 +0100

    404 template

--- serv/app/api/v0/search.py
+++ serv/app/api/v0/search.py
@@ -19,9 +19,12 @@ class SearchView(FlaskView):
 
     @route('/<hash>', methods=['GET','POST'])
     def search(self, hash=None):
-        if request.method == "POST":
+        print("BUSCANDO")
+        if request.method == 'POST':
+            print("HOLA")
             hash = request.data["hash"]
         if hash:
+            print("ADIOS")
             assets = mongo.db.assets
             query = assets.find({"$or": [{"ssdeep": {"$eq": hash}}, {"md5": {"$eq": hash}}, {"sha1": {"$eq": hash}}, {"sha256": {"$eq": hash}},{"sha512": {"$eq": hash}}]})
             if query.count() == 0:
@@ -31,5 +34,6 @@ class SearchView(FlaskView):
                 for q in query:
                     return redirect(url_for("detail.index", hash=q['sha256']))
             else:
+                flash("500 - Hash not found :(", "danger")
                 return abort(500)
         return redirect(url_for("index.index"))
