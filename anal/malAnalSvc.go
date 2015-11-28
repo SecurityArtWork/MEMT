3a2a5747decade4e50e2046a1858e58b314fc00c
José F. González Krause <josef@hackercat.ninja>
2015-11-28 01:54:42 +0100

    Added hashing and finished 'hashing/hashing.go' library

--- anal/malAnalSvc.go
+++ anal/malAnalSvc.go
@@ -1,3 +1,25 @@
+/*
+
+// Test set
+
+{
+	"celeryID": "1234567890",
+	"mongoID": "0987654321",
+	"path": "~/Desktop/memtEnvEmu/uploads/wxp1.exe",
+	"ipMeta": {
+		"ip": "173.194.45.55",
+	    "iso_code": "US",
+	    "country": "United States",
+	    "city": "Minneapolis",
+	    "geo": [-93.2166, 44.9759],
+	    "date": "1448670771"}
+}
+
+
+curl -s http://localhost:31337/ -d'{"celeryID": "1234567890", "mongoID": "0987654321", "path": "/Users/kajuryto/Desktop/memtEnvEmu/uploads/wxp1.exe", "ipMeta": {"ip": "173.194.45.55", "iso_code": "US", "country": "United States", "city": "Minneapolis", "geo": [-93.2166, 44.9759], "date": "1448670771"} }' |python -m json.tool
+
+*/
+
 package main
 
 import (
@@ -13,7 +35,7 @@ import (
 
 	// "github.com/securityartwork/cat/binanal"
 	// "github.com/securityartwork/cat/image"
-	// "github.com/securityartwork/hashing"
+	"github.com/securityartwork/anal/hashing"
 )
 
 const (
@@ -21,30 +43,16 @@ const (
 )
 
 var serviceFlag, infoFlag bool
-var hostFlag, portFlag string
+var hostFlag, portFlag, binDstFlag, imgDstFlag string
 
 func init() {
 	flag.BoolVar(&infoFlag, "-info", false, "Shows about info.")
 	flag.BoolVar(&serviceFlag, "-daemon", false, "Analysis as μservice.")
 	flag.StringVar(&hostFlag, "-host", "127.0.0.1", "Sets the μservice address.")
 	flag.StringVar(&portFlag, "-port", "31337", "Sets the μservice port.")
+	flag.StringVar(&binDstFlag, "-bindst", "/tmp/bin", "Binary final folder.")
+	flag.StringVar(&imgDstFlag, "-imgdst", "/tmp/img", "Images final folder.")
 }
-
-// "ipMeta": [{"ip": "173.194.45.55",
-//             "iso_code": "US",
-//             "country": "United States",
-//             "city": "Minneapolis",
-//             "geo": [-93.2166, 44.9759],
-//             "date": ""}],
-//
-// type IPMeta struct {
-// 	IP      string    `json:"ip"`
-// 	ISOCode string    `json:"iso_code"`
-// 	Country string    `json:"country"`
-// 	City    string    `json:"city"`
-// 	Geo     []float32 `json:"geo"`
-// 	Date    string    `json:"date"`
-// }
 
 type Artifact struct {
 	Ssdeep      string   `json:"ssdeep"`
@@ -143,11 +151,13 @@ func analysisEndpoint(rw http.ResponseWriter, req *http.Request) {
 	// Recover path of the artifact to analyze
 	path := at.Path
 
+	// Generate hashes
 	if err := generateHashes(&artifact, path); err != nil {
 		sendJSONError(rw, http.StatusInternalServerError, err)
 		return
 	}
 
+	// Extract binary info
 	if err := binaryData(&artifact, path); err != nil {
 		sendJSONError(rw, http.StatusInternalServerError, err)
 		return
@@ -165,6 +175,43 @@ func analysisEndpoint(rw http.ResponseWriter, req *http.Request) {
 
 // Generate the hashes of the binary file
 func generateHashes(artifact *Artifact, path string) error {
+	// Read file to byte array
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	ssdeep, err := hashing.SSDEEPFromFile(path)
+	if err != nil {
+		return err
+	}
+
+	md5, err := hashing.CalcMD5(file)
+	if err != nil {
+		return err
+	}
+
+	sha1, err := hashing.CalcSHA1(file)
+	if err != nil {
+		return err
+	}
+
+	sha256, err := hashing.CalcSHA256(file)
+	if err != nil {
+		return err
+	}
+
+	sha512, err := hashing.CalcSHA512(file)
+	if err != nil {
+		return err
+	}
+
+	artifact.Ssdeep = ssdeep
+	artifact.Md5 = md5
+	artifact.Sha1 = sha1
+	artifact.Sha256 = sha256
+	artifact.Sha512 = sha512
+
 	return nil
 }
 
