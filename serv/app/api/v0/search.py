6470a1fa1bf5857be7769668a1ede2235d752b3b
Jaume Martin <jaumartin@gmail.com>
2015-11-28 18:50:18 +0100

    Added API for Malware and Search. Update README.md as well

--- serv/app/api/v0/search.py
+++ serv/app/api/v0/search.py
@@ -4,6 +4,8 @@ This module define every API related to maps.
 """
 from __future__ import print_function, absolute_import
 
+from . import __api_prefix__
+
 from flask import jsonify
 from flask import request
 from flask import redirect
@@ -16,23 +18,47 @@ from flask.ext.classy import FlaskView, route
 from app.extensions import mongo
 
 class SearchView(FlaskView):
+    route_prefix = __api_prefix__
 
-    @route('/', methods=['POST'])
     @route('/<hash>', methods=['GET'])
-    def search(self, hash=None):
-        if request.method == 'POST':
-            hash = request.form["hash"]
+    @route('/<hash>/', methods=['GET'], defaults={'type': 'json'})
+    def search(self, hash, type=None):
         if hash:
-            print("ADIOS")
             assets = mongo.db.assets
             query = assets.find({"$or": [{"ssdeep": {"$eq": hash}}, {"md5": {"$eq": hash}}, {"sha1": {"$eq": hash}}, {"sha256": {"$eq": hash}},{"sha512": {"$eq": hash}}]})
-            if query.count() == 0:
-                flash("404 - Hash not found :(", "danger")
-                return abort(404)
-            elif query.count() == 1:
+            if query.count() == 1:
                 for q in query:
-                    return redirect(url_for("detail.index", hash=q['sha256']))
+                    if type == "json":
+                        return jsonify(res={"ecode": 200, "info": "Found", "data": get_data(q)})
+                    else:
+                        return jsonify(res={"ecode": 200, "info": "Found", "data": get_data(q)})
+            if type == "json":
+                return jsonify(res={"ecode": 404, "info": "Not Found", "data": {}})
             else:
-                flash("500 - Hash not found :(", "danger")
-                return abort(500)
-        return redirect(url_for("index.index"))
+                return jsonify(res={"ecode": 404, "info": "Not Found", "data": {}})
+
+def get_data(malware):
+    obj = {}
+    obj["ssdeep"] = malware["ssdeep"]
+    obj["md5"] = malware["md5"]
+    obj["sha1"] = malware["sha1"]
+    obj["sha256"] = malware["sha256"]
+    obj["sha512"] = malware["sha512"]
+    obj["format"] = malware["format"]
+    obj["symbols"] = malware["symbols"]
+    obj["imports"] = malware["imports"]
+    obj["sections"] = malware["sections"]
+    obj["arch"] = malware["arch"]
+    obj["strain"] = malware["strain"]
+    obj["mutations"] = []
+    obj["siblings"] = []
+    if obj["strain"] == "":  # This is a strain
+        obj["mutations"] = malware["mutations"]
+    else:  # This is a mutation
+        assets = mongo.db.assets
+        strains = assets.find({"sha256": obj["strain"]})
+        for strain in strains:
+            if strain["mutations"] != obj["sha256"]:
+                obj["siblings"].append(strain["mutations"])
+        obj["siblings"].remove(obj["sha256"])
+    return obj
